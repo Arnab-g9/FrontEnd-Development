3f2ff8bdf0ecc54dec252908e6cc91c3ec58b360
Arnabg_9 <arnabghorai15@gmail.com>
2024-02-23 10:58:43 +0530

    day-04

--- React Js/tip-calculator/src/App.js
+++ React Js/tip-calculator/src/App.js
@@ -1,22 +1,75 @@
+import { useState } from "react";
+
 export default function App() {
-  <div></div>;
-}
-function ExpenseTracker() {
   return (
     <div>
-      <span>How much was the bill?</span> <input type="text" />
-      <div>
-        <span>How did you like the service ?</span>
-        <section>
-          <option value={1}></option>
-        </section>
-      </div>
-      <div>
-        <span>How did you like the service ?</span>
-        <section>
-          <option value={1}></option>
-        </section>
-      </div>
+      <TipCalculator />
+    </div>
+  );
+}
+function TipCalculator() {
+  const [amount, setAmount] = useState(0);
+  const [tip1, setTip1] = useState("");
+  const [tip2, setTip2] = useState("");
+
+  const avgTip = (Number(tip1) + Number(tip2)) / 2;
+  const tip = (amount * avgTip) / 100;
+  const total = amount + tip;
+  function onAmountChange(e) {
+    setAmount(() => Number(e.target.value));
+  }
+  function handleReset() {
+    setAmount(0);
+    setTip1("");
+    setTip2("");
+  }
+  return (
+    <div>
+      <Amount amount={amount} onAmountChange={onAmountChange} />
+      <Tip tip={tip1} onChangeTip={(e) => setTip1(Number(e.target.value))}>
+        <span>How do you like the service? </span>
+      </Tip>
+      <Tip tip={tip2} onChangeTip={(e) => setTip2(Number(e.target.value))}>
+        <span>How do your friend like the service? </span>
+      </Tip>
+      <Output tip={tip} total={total} amount={amount} />
+      <Reset onReset={handleReset} />
+    </div>
+  );
+}
+
+function Amount({ amount, onAmountChange }) {
+  return (
+    <div>
+      <span>How much was the bill</span>
+      <input type="text" value={amount} onChange={onAmountChange} />
+    </div>
+  );
+}
+function Tip({ tip, onChangeTip, children }) {
+  return (
+    <div>
+      {children}
+      <select value={tip} onChange={onChangeTip}>
+        <option value="0">Dissatisfied(0%)</option>
+        <option value="5">it was okey(5%)</option>
+        <option value="10">it was good (10%)</option>
+        <option value="20">Absolutely amazing(20%)</option>
+      </select>
+    </div>
+  );
+}
+function Output({ tip, total, amount }) {
+  return (
+    <div>
+      you pay {total} (${amount} + ${tip})
+    </div>
+  );
+}
+function Reset({ onReset }) {
+  return (
+    <div>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
 }
