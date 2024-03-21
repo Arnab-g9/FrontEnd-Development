931b702e5b2d06177506a7399128f8269234fa68
Arnabg_9 <arnabghorai15@gmail.com>
2024-03-21 08:28:36 +0530

    usePopcorn

--- React Js/usepopcorn/src/index.js
+++ React Js/usepopcorn/src/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css" ;
 // import App from "./App";
 
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,8 +22,9 @@ root.render(
     {/* <App /> */}
     <StarRating
       maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      messages={["Terri ble", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
