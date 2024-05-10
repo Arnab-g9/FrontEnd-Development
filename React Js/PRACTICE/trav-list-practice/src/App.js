758e92884833cbbd34dfdf67246a23b263013e0e
Arnabg_9 <arnabghorai15@gmail.com>
2024-05-10 07:45:55 +0530

    init

--- React Js/PRACTICE/trav-list-practice/src/App.js
+++ React Js/PRACTICE/trav-list-practice/src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "./index.css";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 export default function App() {
   return (
@@ -18,10 +19,33 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [qty, setQty] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do u need for your 😃 trip?</h3>
-    </div>
+      <select value={qty} onChange={(e) => setQty(Number(e.target.value))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -29,14 +53,22 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 function Item({ item }) {
-  return <li>{item.description}</li>;
+  return (
+    <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
+        {item.quantity + " "}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
