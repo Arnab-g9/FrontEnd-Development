dc41518fe00726097d9934168748cd31e4b45844
Arnabg_9 <arnabghorai15@gmail.com>
2024-05-29 11:48:26 +0530

    init

--- React Js/PRACTICE/currency-converter/src/App.js
+++ React Js/PRACTICE/currency-converter/src/App.js
@@ -10,14 +10,21 @@ export default function App() {
   useEffect(
     function () {
       async function fetchCurrency() {
-        const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=${amount}&from=${selectCurrency}&to=${convertCurrency}`
-        );
-        const data = await res.json();
-        setConversion(() => data.rates[convertCurrency]);
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${selectCurrency}&to=${convertCurrency}`
+          );
+          const data = await res.json();
+          setConversion(() => data.rates[convertCurrency]);
+        } catch (error) {
+          console.error("Error fetching currency data: ", error);
+          setConversion(0);
+        }
       }
-      if (amount < 1) return;
-      else if (selectCurrency === convertCurrency) {
+      if (amount <= 0) {
+        setConversion(0);
+        return;
+      } else if (selectCurrency === convertCurrency) {
         setConversion(amount);
         return;
       }
@@ -26,23 +33,33 @@ export default function App() {
     [amount, selectCurrency, convertCurrency]
   );
   return (
-    <div>
-      <SelectCurrency
-        currency={selectCurrency}
-        onChangeCurrency={setSelectCurrency}
-      />
-      <InputValue amount={amount} onChangeAmount={setAmount} />
-      <SelectCurrency
-        currency={convertCurrency}
-        onChangeCurrency={setConvertCurrency}
-      />
-      <DisplayMessage conversion={conversion} />
+    <div className="app">
+      <Header />
+      <div className="form-container">
+        <div className="input-container">
+          <SelectCurrency
+            currency={selectCurrency}
+            onChangeCurrency={setSelectCurrency}
+          />
+          <InputValue amount={amount} onChangeAmount={setAmount} />
+          <SelectCurrency
+            currency={convertCurrency}
+            onChangeCurrency={setConvertCurrency}
+          />
+        </div>
+
+        <DisplayMessage conversion={conversion} />
+      </div>
     </div>
   );
 }
 function SelectCurrency({ currency, onChangeCurrency }) {
   return (
-    <select value={currency} onChange={(e) => onChangeCurrency(e.target.value)}>
+    <select
+      value={currency}
+      onChange={(e) => onChangeCurrency(e.target.value)}
+      className="btn"
+    >
       <option value="USD">USD</option> <option value="EUR">EUR</option>{" "}
       <option value="CAD">CAD</option> <option value="INR">INR</option>
     </select>
@@ -50,16 +67,26 @@ function SelectCurrency({ currency, onChangeCurrency }) {
 }
 
 function InputValue({ amount, onChangeAmount }) {
-  return (
-    <input
-      type="text"
-      value={amount}
-      onChange={(e) => onChangeAmount(Number(e.target.value))}
-    />
-  );
+  const handleChange = (e) => {
+    const value = parseFloat(e.target.value);
+    if (isNaN(value)) {
+      onChangeAmount(0);
+    } else {
+      onChangeAmount(value);
+    }
+  };
+  return <input type="text" value={amount} onChange={handleChange} />;
 }
 
 function DisplayMessage({ conversion }) {
   console.log(conversion);
-  return <p>{conversion}</p>;
+  return (
+    <div className="messege-container">
+      <p className="messege">{conversion}</p>
+    </div>
+  );
+}
+
+function Header() {
+  return <header>Currency Converter</header>;
 }
